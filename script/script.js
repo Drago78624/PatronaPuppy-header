4a82768e40e5be2cd1a2ef331bdb5efe4b358316
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2021-12-16 18:38:15 +0500

    with dropdowns

---
+++ script/script.js
@@ -0,0 +1,13 @@
+document.getElementById("hamburger").addEventListener("click", () => {
+  const menuDisplay = document.getElementById("menu");
+  //   if (menuDisplay.style.display == "none") {
+  //     menuDisplay.classList.toggle("u-display-block");
+  //   } else if (menuDisplay.style.display == "block") {
+  //     menuDisplay.classList.toggle("u-display-block");
+  //   }
+  //   console.log(menuDisplay);
+  menuDisplay.classList.toggle("u-display-block");
+    
+});
+
+
