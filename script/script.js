4db57957dc0599344dc9072dd1bc2fe1075e15df
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2021-12-17 19:54:39 +0500

    honolulu

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 document.getElementById("hamburger").addEventListener("click", () => {
   const menuDisplay = document.getElementById("menu");
+  const dropdownDisplay = document.querySelector(".navbar-dropdown__option--menu");
   //   if (menuDisplay.style.display == "none") {
   //     menuDisplay.classList.toggle("u-display-block");
   //   } else if (menuDisplay.style.display == "block") {
@@ -7,7 +8,16 @@ document.getElementById("hamburger").addEventListener("click", () => {
   //   }
   //   console.log(menuDisplay);
   menuDisplay.classList.toggle("u-display-block");
+  dropdownDisplay.classList.remove("u-display-none");
     
 });
+
+document.getElementById("blog").addEventListener("click", () => {
+  const dropdownDisplay = document.querySelector(".navbar-dropdown__option--menu");
+  dropdownDisplay.classList.toggle("u-display-none");
+  document.getElementById("blog").classList.toggle("u-color-black")
+    
+});
+
 
 
